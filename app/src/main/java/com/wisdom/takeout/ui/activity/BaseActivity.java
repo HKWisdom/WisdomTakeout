fb6b7e0fc5df475487dacdc8a06039f6898b4842
hk <hk12345>
2017-03-19 17:54:35 +0800

    first commit

---
+++ app/src/main/java/com/wisdom/takeout/ui/activity/BaseActivity.java
@@ -0,0 +1,10 @@
+package com.wisdom.takeout.ui.activity;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by HKWisdom on 2017/3/19.
+ */
+
+public class BaseActivity extends AppCompatActivity {
+}
